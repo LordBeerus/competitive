5adddf49859cf73185fb779d523fd8eab7d03128
Yashashavi <yash1995.momyan@gmail.com>
2016-07-10 17:29:13 +0530

    Create Header Template 2.cpp

---
+++ Header Template 2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define all(b) b.begin() , b.end()
+#define MP(a, b) make_pair(a, b)
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+template<class T>
+int size(const T &x) { return x.size(); }
+double INF = 1e30, eps = 1e-6;
+int sig(double x) { return abs(x) < eps ? 0 : (x > 0 ? 1 : -1); }
+typedef long double DOUBLE;
+typedef vector<DOUBLE> VD;
+typedef vector<VD> VVD;
+typedef vector<int> VI;
+#define rep(i, a, b) for (__typeof(a) i=(a); i<(b); ++i)
+#define iter(it, c) for (__typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define ii pair<int,int>
+#define turnOff(k, i) (k&(~(1<<(i-1))))
+#define pll pair<long long int , long long int>
+#define ull unsigned long long int
+#define ll  long long int
+#define MAXN 502
+#define MOD 1000000007
+#define INF 1000000007
+#define MAXLG 12
+#define EPS 0.00000001
