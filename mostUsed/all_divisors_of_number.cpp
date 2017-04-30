63d577a5ecad56de2ad3e2a3760241e9b333edfb
Yashashavi <yash1995.momyan@gmail.com>
2017-04-30 10:08:03 +0530

    some changes

--- mostUsed/all_divisors_of_number.cpp
+++ mostUsed/all_divisors_of_number.cpp
@@ -2,15 +2,13 @@ vi getAllDivisors(int num){
      vector<int> divs;
     int square_root = (int) sqrt(num) + 1;
     for (int i = 1; i < square_root; i++) {
-        if (num % i == 0&&i*i!=num)
-        {
+        if (num % i == 0)
+        {  if(i*i!=num) {
             divs.pb(i);
+        }
             divs.pb(num/i);
         }
-        if (num % i == 0&&i*i==num)
-        {
-            divs.pb(i);
-        }
+       
     }
     return divs;
 }
