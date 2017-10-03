d2cbd4737ce28a0fbead575e833ca952ce8dc464
Yashashavi <yash1995.momyan@gmail.com>
2017-10-04 01:24:37 +0530

    Update powerFunc and combination(C{n,k}).cpp

--- mostUsed/powerFunc and combination(C{n,k}).cpp
+++ mostUsed/powerFunc and combination(C{n,k}).cpp
@@ -11,6 +11,34 @@ long mod_pow(long x, int y)
     }
     return r;
 }
+
+ll perm[MAXN];
+ll invPerm[MAXN];
+ll mod_pow(ll x, ll y)
+{
+    // exponentiation by squaring
+    ll r = 1;
+    while (y > 0) {
+        if (y % 2 == 1) {
+            r = (r*x) % MOD;
+        }
+        x = (x*x) % MOD;
+        y /= 2;
+    }
+    return r%MOD;
+}
+inline ll comb(int n,int k) {
+    return (((perm[n]*invPerm[k])%MOD)*invPerm[n-k])%MOD;
+}
+
+perm[0] = 1;
+    invPerm[0] = 1;
+    rep (i,1,MAXN) {
+        perm[i] = (perm[i-1]*i)%MOD;
+        invPerm[i] = mod_pow(perm[i],MOD-2)%MOD;
+    }
+
+//-----
   vector<vector<long>> C(n+1, vector<long>(n+1));
     for (int i = 0; i <= n; i++) {
         C[i][0] = 1;
