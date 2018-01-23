b02cd5075a09759131fb255b64f186b65422ee33
Yashashavi <yash1995.momyan@gmail.com>
2018-01-23 23:04:23 +0530

    range update added

--- mostUsed/segment_tree_base.cpp
+++ mostUsed/segment_tree_base.cpp
@@ -1,26 +1,43 @@
 int vert[4*MAXN];
-int add (int val,int pos,  int node, int l ,int r) {
+//point update
+void add (int val,int pos,  int node, int l ,int r) {
 
     if (l == r) {
         vert[node] = val;
-        return vert[node];
+        return  ;
     }
     int mid = (l + r)>>1;
-    int m = INF;
+    
     if (pos<= mid) m = add (val, pos, node<<1 , l , mid);
     else m = add (val, pos, (node<<1) + 1, mid + 1 , r);
 
-    vert[node] = min (vert[node] , m);
+    vert[node] =  // ;
     return vert[node];
 }
-
+//range query
 int query(int qL,int qR, int num = 1, int l = 0, int r = MAXN -1 ) {
     if (qL <=l && qR>= r ) return vert[num];
     int mid = (l + r)>>1;
     int ans = INF;
-    if (qL <= mid) ans = min (query(qL, qR, num<<1 , l , mid), ans);
-    if (qR >= mid + 1) ans = min (query(qL, qR, (num<<1)+ 1, mid + 1 , r), ans);
+    if (qL <= mid) ans =  query(qL, qR, num<<1 , l , mid) ;
+    if (qR >= mid + 1) ans = query(qL, qR, (num<<1)+ 1, mid + 1 , r) ;
 
     return ans;
+
+}
+//range update 
+void add (int val,int L,int R,  int node, int l ,int r) {
+    if (l>=L && r<=R) {
+        lazy[node] += val;
+        vert[node] += val;
+        return;
+    }
+    push(node);
+    int mid = (l + r)>>1;
+
+    if (L<= mid)  add (val, L,R, node<<1 , l , mid);
+     if (R>mid) add (val, L,R, (node<<1) + 1, mid + 1 , r);
+
+    vert[node] = vert[left(node)]+vert[right(node)];
 
 }
