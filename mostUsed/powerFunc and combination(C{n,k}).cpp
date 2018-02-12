1e6d793a2be764ebf2490b7aae094a3d6756b99a
Yashashavi <yash1995.momyan@gmail.com>
2018-02-12 16:03:08 +0530

    Update powerFunc and combination(C{n,k}).cpp

--- mostUsed/powerFunc and combination(C{n,k}).cpp
+++ mostUsed/powerFunc and combination(C{n,k}).cpp
@@ -3,38 +3,39 @@ ll mod_pow(ll x, ll y) {
     ll r = 1;
     while (y > 0) {
         if (y % 2 == 1) {
-            r = (r * x) % MOD;
+            r = (r * x) % mod;
         }
-        x = (x * x) % MOD;
+        x = (x * x) % mod;
         y /= 2;
     }
     return r;
 }
 
-ll perm[MAXN];
-ll invPerm[MAXN];
+ll perm[maxn];
+ll invPerm[maxn];
 ll mod_pow(ll x, ll y)
 {
     // exponentiation by squaring
     ll r = 1;
     while (y > 0) {
         if (y % 2 == 1) {
-            r = (r*x) % MOD;
+            r = (r*x) % mod;
         }
-        x = (x*x) % MOD;
+        x = (x*x) % mod;
         y /= 2;
     }
-    return r%MOD;
+    return r%mod;
 }
 inline ll comb(int n,int k) {
-    return (((perm[n]*invPerm[k])%MOD)*invPerm[n-k])%MOD;
+    if (k>n)return 0;
+    return (((perm[n]*invPerm[k])%mod)*invPerm[n-k])%mod;
 }
 
 perm[0] = 1;
     invPerm[0] = 1;
-    rep (i,1,MAXN) {
-        perm[i] = (perm[i-1]*i)%MOD;
-        invPerm[i] = mod_pow(perm[i],MOD-2)%MOD;
+    rep (i,1,maxn) {
+        perm[i] = (perm[i-1]*i)%mod;
+        invPerm[i] = mod_pow(perm[i],MOD-2)%mod;
     }
 
 //-----
@@ -42,6 +43,6 @@ perm[0] = 1;
     for (int i = 0; i <= n; i++) {
         C[i][0] = 1;
         for (int j = 1; j <= i; j++) {
-            C[i][j] = (C[i-1][j-1] + C[i-1][j]) % MOD;
+            C[i][j] = (C[i-1][j-1] + C[i-1][j]) % mod;
         }
     }
